3b86204ee43e7f523a077a9a2e69a45761bde189
yihtserns <sim_yihtsern@yahoo.com>
2016-02-22 17:08:46 +0800

    Basic impl of Groovy Decorator.

---
+++ src/main/java/com/github/yihtserns/groovy/decorator/DecoratorASTTransformation.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2016 yihtserns.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.yihtserns.groovy.decorator;
+
+import org.codehaus.groovy.ast.ASTNode;
+import org.codehaus.groovy.ast.AnnotationNode;
+import org.codehaus.groovy.ast.ClassHelper;
+import org.codehaus.groovy.ast.MethodNode;
+import org.codehaus.groovy.ast.VariableScope;
+import org.codehaus.groovy.ast.expr.ClosureExpression;
+import org.codehaus.groovy.ast.stmt.BlockStatement;
+import org.codehaus.groovy.ast.stmt.Statement;
+import org.codehaus.groovy.ast.tools.GeneralUtils;
+import static org.codehaus.groovy.ast.tools.GeneralUtils.callX;
+import static org.codehaus.groovy.ast.tools.GeneralUtils.closureX;
+import static org.codehaus.groovy.ast.tools.GeneralUtils.returnS;
+import org.codehaus.groovy.control.CompilePhase;
+import org.codehaus.groovy.control.SourceUnit;
+import org.codehaus.groovy.transform.ASTTransformation;
+import org.codehaus.groovy.transform.GroovyASTTransformation;
+
+/**
+ *
+ * @author yihtserns
+ */
+@GroovyASTTransformation(phase = CompilePhase.SEMANTIC_ANALYSIS)
+public class DecoratorASTTransformation implements ASTTransformation {
+
+    @Override
+    public void visit(ASTNode[] astNodes, final SourceUnit sourceUnit) {
+        AnnotationNode annotation = (AnnotationNode) astNodes[0];
+
+        DecoratorClass decoratorClass = (DecoratorClass) annotation.getClassNode().getTypeClass().getAnnotation(DecoratorClass.class);
+
+        MethodNode method = (MethodNode) astNodes[1];
+        BlockStatement originalCode = (BlockStatement) method.getCode();
+
+        ClosureExpression closuredOriginalCode = closureX(originalCode);
+        closuredOriginalCode.setVariableScope(new VariableScope());
+
+        Statement[] statements = {
+            returnS(callX(ClassHelper.make(decoratorClass.value()), "call", GeneralUtils.args(closuredOriginalCode)))
+        };
+        method.setCode(new BlockStatement(statements, originalCode.getVariableScope()));
+    }
+}
