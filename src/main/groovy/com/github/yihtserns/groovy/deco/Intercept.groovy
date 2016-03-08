ff4246d3bae65814d41074cd8490534f9ca85c74
yihtserns <sim_yihtsern@yahoo.com>
2016-03-09 02:18:11 +0800

    Add convenience annotation that allows interception to be coded on the fly.

---
+++ src/main/groovy/com/github/yihtserns/groovy/deco/Intercept.groovy
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2016 yihtserns.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.yihtserns.groovy.deco
+
+import java.lang.annotation.Retention
+import java.lang.annotation.RetentionPolicy
+import java.lang.annotation.Target
+import java.lang.annotation.ElementType
+import org.codehaus.groovy.transform.GroovyASTTransformationClass
+
+/**
+ * @see #value()
+ * @author yihtserns
+ */
+@MethodDecorator({ func, Intercept intercept ->
+    def handle = intercept.value().newInstance(this, this)
+
+    return { args ->
+        handle(func, args)
+    }
+})
+@GroovyASTTransformationClass("com.github.yihtserns.groovy.deco.DecoratorASTTransformation")
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@interface Intercept {
+
+    Class<? extends Closure> value()
+}
